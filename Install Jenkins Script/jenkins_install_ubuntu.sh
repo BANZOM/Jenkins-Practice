185fd0afe3e57ee4f60862cceca2e6e0adc37c47
banzom <aditya.panwar.2208@gmail.com>
2024-07-07 16:16:49 +0530

    feat: Add script to install and configure Jenkins on Ubuntu

---
+++ Install Jenkins Script/jenkins_install_ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+
+# Remove comment below to install java 17
+# sudo apt install openjdk-17-jdk -y
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update
+
+sudo apt install jenkins -y
+
+sudo systemctl start jenkins.service
+
+sudo systemctl enable jenkins.service
